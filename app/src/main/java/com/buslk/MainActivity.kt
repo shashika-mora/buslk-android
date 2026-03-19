51e182d5b41b554b96bf0c8fa59fcddbbcf87376
Raveesha Sandamini <raveeshasandamini20041208@gmail.com>
2026-03-19 22:43:12 +0530

    Import additional Material icons for search and UI features

--- app/src/main/java/com/buslk/MainActivity.kt
+++ app/src/main/java/com/buslk/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
