d247e0ba68d86a1f37e5b66e2e966a7f6c37c3b9
Raveesha Sandamini <raveeshasandamini20041208@gmail.com>
2026-03-21 00:24:40 +0530

    Create ChatScreen structure with Scaffold

---
+++ app/src/main/java/com/buslk/ui/screens/ChatScreen.kt
@@ -0,0 +1,18 @@
+package com.buslk.ui.screens
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatScreen(
+    friendName: String,
+    onBackClick: () -> Unit
+) {
+    Scaffold(
+        topBar = {},
+        bottomBar = {}
+    ) { paddingValues ->
+
+    }
+}
